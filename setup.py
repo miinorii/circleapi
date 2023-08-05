1443d5a0222f68685e52c9085d8b9581d88b8397
miinorii <77248345+miinorii@users.noreply.github.com>
2023-08-05 13:02:46 +0200

    update setup py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup
 setup(
     name="circleapi",
     author="miinorii",
-    version="2023.7.15",
+    version="2023.8.5",
     packages=["circleapi"],
+    description="osu! apiv2 python wrapper",
+    long_description="osu! apiv2 python wrapper",
+    keywords=["osu!", "apiv2", "wrapper"],
+    url="https://github.com/miinorii/circleapi",
     install_requires=[
         "httpx[http2]>=0.24.1",
         "pydantic>=2.1.1",
