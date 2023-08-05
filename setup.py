9ec6ee1573c1c0d025ee719453832a5bb3d66c68
miinorii <77248345+miinorii@users.noreply.github.com>
2023-08-05 12:53:21 +0200

    bump dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=["circleapi"],
     install_requires=[
         "httpx[http2]>=0.24.1",
-        "pydantic>=2.0",
+        "pydantic>=2.1.1",
         "python-dotenv>=1.0.0"
     ],
     python_requires=">=3.10"
